358c0e1e0d2d41adbdd7f2acf9e494eb827e2cfb
kennarachael <142414095+kennarachael@users.noreply.github.com>
2025-04-19 00:10:12 -0400

    complete!

--- ppmedit.py
+++ ppmedit.py
@@ -1,46 +1,52 @@
 
 class PPMimage:
     
-    def __init__(self, file, outfile):
+    def __init__(self, infile, outfile):
         
         self.output_file_name = outfile
-        self.image = self.setup(file)
-        self.attributes = [] #list
-    
+        self.image = self.setup(infile)
+        self.attributes = self.find_attributes(infile) #list
+
 
     def out(self,file):
-        
-        print(self.attributes[0] + "\n" + self.attributes[1] + " " + self.attributes[2] + "\n" + self.attributes[3]+ "\n")
+        o = open(self.output_file_name, "w+")
+        o.write(str(self.attributes[0]) + "\n" + "\t" + str(self.attributes[1]) + " " + str(self.attributes[2]) + "\n" + "\t" + str(self.attributes[3]) + "\n")
+
         for row in file:
             for pixel in row:
+                o.write("\t")
                 for val in pixel:
-                    print(val + " ")
-            print("\n") 
+                    o.write(str(val) + " ")
+            o.write("\n") 
 
+        o.read()
+        return o
 
-
+    def open(self,file):
+        infile = open(file, "r")
+        nimage = infile.read()
+        image = nimage.split()
+        return image
 
     def setup(self,file):
         #read file and turn into list
-        #infile = open(file, "r")
-        #nimage = infile.read()
-        image = file.split() #image = nimage.split()
-        
-        self.attributes = self.find_attributes(image)
-
-        nimage = image[3:]
+        image = self.open(file)
+        #print(image)
+        self.attributes = self.find_attributes(file)
+        nimage = image[4:] 
         nimage = self.format(nimage)
 
         return nimage
         
 
     def find_attributes(self,file):
-        magic_num = file[0] 
-        columns = file[1]
-        rows = file[2]
-        max_colour = file[3]
+        pix = self.open(file)
+        self.magic_num = pix[0]
+        self.columns = pix[1]
+        self.rows = pix[2]
+        self.max_colour = pix[3]
         
-        return [magic_num, columns, rows, max_colour]
+        return [self.magic_num, self.columns, self.rows, self.max_colour]
     
 
     def copy(self,file):
@@ -51,19 +57,20 @@ class PPMimage:
     def format(self,file):
         #formats the file in a way that makes transformations easier (3d list)
         list = []
-        
+        row = []
+        pixel = []
+
         for val in file:
-            row = []
-            pixel = []
-            pixel += val
+            
+            pixel.append(val)
             #after 3, void pixel list after adding it to row
             if len(pixel) == 3:
                 row.append(pixel)
                 pixel = []
 
             #below checks if the row is complete based on columns amount given (attribute)
-            if len(row) == self.attributes[1]:
-                list.append[row]
+            if len(row) == int(self.attributes[1]):
+                list.append(row)
                 row = []
         
         return list
@@ -71,22 +78,23 @@ class PPMimage:
 
 
     def negate_red(self):
-        nimage = self.copy(self.image)
+        negimage = self.copy(self.image)
         #make a copy of self.image first and only makes changes to the copy
-        for row in nimage:
+        for row in negimage:
             for pixel in row:
-                red = pixel[0]
-                neg_red = reversed(red)
-                red = neg_red
-        return nimage
+                red = int(pixel[0])
+                neg_red = 255 - red
+                pixel[0] = neg_red
+                
+        self.out(negimage)
 
     def flip_horizontal(self):
         flipimage = self.copy(self.image)
         for row in flipimage:
-            row.replace(row.reverse())
-        return flipimage
+            row = (row.reverse())
+        self.out(flipimage)
 
-    def grey_scale(self, file):
+    def grey_scale(self):
         grey = self.copy(self.image)
 
         
@@ -95,36 +103,31 @@ class PPMimage:
                 total = 0
                 count = 0
                 for val in pixel:
-                    total +=val
+                    total +=int(val)
                     count +=1
                     if count ==3:
-                        num = total /3
-                        pixel = [num,num,num]
-                total = 0
-                count = 0
+                        num = total // 3
+                        pixel[0] = num
+                        pixel[1] = num
+                        pixel[2] = num
 
-        return grey
+                        count = 0
+                        total = 0
+                    
+                
+                
+
+        self.out(grey)
     
-
-    def flatten_red(self,file):
+    def flatten_red(self):
         flat = self.copy(self.image)
 
         for row in flat:
             for pixel in row:
                 pixel[0] = 0
         
-        return flat
+        self.out(flat)
 
 
 
-c = PPMimage("tinypix.ppm","out.ppm")
-#print(c.image)
-#print(c.attributes)
-
-print(c.setup("P3\
-    4 4\
-    255\
-    0  0  0   100 0  0       0  0  0    255   0 255\
-    0  0  0    0 255 175     0  0  0     0    0  0\
-    0  0  0    0  0  0       0 15 175    0    0  0\
-    255 0 255  0  0  0       0  0  0    255  255 255"))
+c = PPMimage("tinypix.ppm","outpix.ppm")
